3dae945a8e3c4b467c5fd66dad9553a606d516f2
Michael Lindstrom <michaellindstr@gmail.com>
2017-10-05 12:38:08 -0500

    added spacecraft

--- planets.rb
+++ planets.rb
@@ -1,5 +1,7 @@
 planet_list = ["Mercury", "Mars"]
 
+space_craft = [['Cassini', 'Saturn', 'Mars'],['Cassini2', 'Earth'],['Cassini3', 'Mars'],["Cassini4","Jupiter", "Mars"]]
+
 planet_list.push("Jupiter", "Saturn")
 
 planet_list.concat(["Uranus", "Neptune"])
@@ -12,14 +14,29 @@ the_rockies = planet_list.slice(0, 4)
 
 planet_list.pop()
 
-puts '-------------'
-puts "Planet List"
-puts '-------------'
+# puts '-------------'
+# puts "Planet List"
+# puts '-------------'
 
-puts planet_list
+# puts planet_list
 
-puts '-------------'
-puts "The Rockies"
-puts '-------------'
+# puts '-------------'
+# puts "The Rockies"
+# puts '-------------'
 
-puts the_rockies
+# puts the_rockies
+
+# p planet_list
+p space_craft
+
+planet_list.each do |planet|
+    plan = "#{planet} visited by: "
+    for craft in space_craft do
+        # p craft
+        if craft.include?(planet)
+            plan += "#{craft[0]} "
+        end
+    end
+    puts plan
+end
+
